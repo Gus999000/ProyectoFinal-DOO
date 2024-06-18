b0f667dc3ee1a29b9746204dcdeb749dd2d3efb3
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-17 22:25:08 -0400

    Cambio en el layout.

--- src/main/java/parte_gráfica/VentanaAplicación.java
+++ src/main/java/parte_gráfica/VentanaAplicación.java
@@ -1,14 +1,15 @@
 package parte_gráfica;
 import javax.swing.*;
+import java.awt.*;
 /**Ventana inicial de la aplicación
  * @author Gustavo González
- * @version versión 1, 10 de junio 2024*/
+ * @version versión 2, 17 de junio 2024*/
 public class VentanaAplicación extends JFrame {
     public VentanaAplicación() {
         super();
         this.setSize(1500, 1000);
         this.setTitle("Aplicación");
-        this.setLayout(null);
+        this.setLayout(new FlowLayout());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
