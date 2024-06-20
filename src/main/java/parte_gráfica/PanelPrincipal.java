f96dbb7a73b7f19ba2b48d14b27d99a8e4613ecf
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-19 23:00:14 -0400

    Modificaciones a como se trabajan los componentes (añadido el panel principal y como se trabaja).

---
+++ src/main/java/parte_gráfica/PanelPrincipal.java
@@ -0,0 +1,20 @@
+package parte_gráfica;
+import parte_lógica.*;
+import javax.swing.*;
+import java.awt.*;
+/**Panel principal de la aplicación
+ * @author Gustavo González
+ * @version 1, 19 de junio de 2024*/
+public class PanelPrincipal extends JPanel {
+    /**Constructor del panel principal*/
+    public PanelPrincipal () {
+        super();
+        this.setLayout(new FlowLayout());
+        this.add(new PanelNaveEspacial(new Apolo11("13:32 P.M", "Luna")));
+        this.add(new PanelNaveEspacial(new ArcaDelVacío("07:21 A.M", "Marte")));
+        this.add(new PanelNaveEspacial(new HalcónMilenario("17:07 P.M", "Corellia")));
+        this.add(new PanelNaveEspacial(new MorningStar("20:07 P.M", "Tierra")));
+        this.add(new PanelNaveEspacial(new UESContactLight("06:21 A.M", "Petrichor V")));
+        this.setBackground(Color.white);
+    }
+}
