16daecd7aac9f747d666c02aa6af8576e0e12e93
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-19 21:21:08 -0400

    Ordenados los modelos.

--- src/main/java/parte_lógica/Modelos.java
+++ src/main/java/parte_lógica/Modelos.java
@@ -1,14 +1,14 @@
 package parte_lógica;
 /**Categorías de asiento
  * @author Gustavo González
- * @version versión 4, 19 de junio 2024*/
+ * @version versión 5, 19 de junio 2024*/
 public enum Modelos {
     /**Constantes de la enumeración (modelos de naves espaciales)*/
-    UES_CONTACT_LIGHT("UES Contact Light"),
+    APOLO_11("Apolo 11"),
     ARCA_DEL_VACÍO("Arca del Vacío"),
     HALCÓN_MILENARIO("Halcón Milenario"),
-    APOLO_11("Apolo 11"),
-    MORNING_STAR("Morning Star");
+    MORNING_STAR("Morning Star"),
+    UES_CONTACT_LIGHT("UES Contact Light");
     /**Modelo de la nave*/
     private final String modelo;
     /**Constructor de Modelos
