465470554afb20191ed7d96b7d4b70b1b221f1be
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-19 21:16:07 -0400

    Añadidos todos los modelos de las naves.

--- src/main/java/parte_lógica/Modelos.java
+++ src/main/java/parte_lógica/Modelos.java
@@ -1,11 +1,14 @@
 package parte_lógica;
 /**Categorías de asiento
  * @author Gustavo González
- * @version versión 3, 17 de junio 2024*/
+ * @version versión 4, 19 de junio 2024*/
 public enum Modelos {
     /**Constantes de la enumeración (modelos de naves espaciales)*/
     UES_CONTACT_LIGHT("UES Contact Light"),
-    ARCA_DEL_VACÍO("Arca del Vacío");
+    ARCA_DEL_VACÍO("Arca del Vacío"),
+    HALCÓN_MILENARIO("Halcón Milenario"),
+    APOLO_11("Apolo 11"),
+    MORNING_STAR("Morning Star");
     /**Modelo de la nave*/
     private final String modelo;
     /**Constructor de Modelos
