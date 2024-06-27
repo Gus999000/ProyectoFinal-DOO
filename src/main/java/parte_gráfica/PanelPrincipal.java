b21569c0001d94c1ac60a96c06a1a6afbb1aa74c
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-26 23:31:30 -0400

    Añadidos los horarios extra.

--- src/main/java/parte_gráfica/PanelPrincipal.java
+++ src/main/java/parte_gráfica/PanelPrincipal.java
@@ -4,17 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 /**Panel principal de la aplicación
  * @author Gustavo González
- * @version 2, 20 de junio de 2024*/
+ * @version 3, 26 de junio de 2024*/
 public class PanelPrincipal extends JPanel {
     /**Constructor del panel principal*/
     public PanelPrincipal () {
         super();
         this.setLayout(new FlowLayout());
-        this.add(new PanelNaveEspacial(new Apolo11("13:32 P.M", "Luna")));
-        this.add(new PanelNaveEspacial(new ArcaDelVacío("07:21 A.M", "Marte")));
-        this.add(new PanelNaveEspacial(new HalcónMilenario("17:07 P.M", "Corellia")));
-        this.add(new PanelNaveEspacial(new MorningStar("20:07 P.M", "Tierra")));
-        this.add(new PanelNaveEspacial(new UESContactLight("06:21 A.M", "Petrichor V")));
+        this.add(new PanelNaveEspacial(new Apolo11("13:32 P.M", "18:07 P.M", "21:22 P.M", "Luna")));
+        this.add(new PanelNaveEspacial(new ArcaDelVacío("07:21 A.M", "14:15 P.M", "20:07 P.M", "Marte")));
+        this.add(new PanelNaveEspacial(new HalcónMilenario("07:06 A.M", "17:07 P.M", "19:07 P.M", "Corellia")));
+        this.add(new PanelNaveEspacial(new MorningStar("07:21 A.M", "11:00 A.M", "20:07 P.M", "Tierra")));
+        this.add(new PanelNaveEspacial(new UESContactLight("06:21 A.M", "11:22 A.M", "18:22 P.M", "Petrichor V")));
     }
     @Override
     public void paintComponent(Graphics g) {
