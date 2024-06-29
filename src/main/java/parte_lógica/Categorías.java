24799f89747d6848835099afdf4d08a00846a3c8
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-29 01:25:40 -0400

    Añadidas mas categorías de asientos.

--- src/main/java/parte_lógica/Categorías.java
+++ src/main/java/parte_lógica/Categorías.java
@@ -1,11 +1,13 @@
 package parte_lógica;
 /**Categorías de asiento
  * @author Gustavo González
- * @version versión 2, 17 de junio 2024*/
+ * @version versión 3, 29 de junio 2024*/
 public enum Categorías {
     /**Constantes de la enumeración (categorías de asientos)*/
-    SEMI_CAMA("semi cama", 5000),
-    SALÓN_CAMA("salón cama", 10000);
+    SEMI_CAMA("Semi cama", 5000),
+    SALÓN_CAMA("Salón cama", 10000),
+    NORMAL("Normal", 1000),
+    MASAJEADOR("Masajeador", 15000);
     /**Categoría del asiento*/
     private final String categoría;
     /**Precio del asiento*/
