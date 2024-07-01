dc953c0d3166c2f5c6c9336712c968f2851591d1
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-30 21:51:26 -0400

    Añadidos los interiores de las naves espaciales.

---
+++ src/main/java/parte_gráfica/PanelInterior.java
@@ -0,0 +1,58 @@
+package parte_gráfica;
+import parte_lógica.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+/**Panel que representa el interior de las naves espaciales
+ * @author Gustavo González
+ * @version versión 1, 30 de junio 2024*/
+public class PanelInterior extends JPanel {
+    /**Instancia lógica de la nave espacial*/
+    private NaveEspacial n;
+    /***/
+    private ArrayList<PanelAsiento> PanelesAsientos = new ArrayList<>();
+    /**Constructor de PanelInterior*/
+    public PanelInterior(NaveEspacial n) {
+        this.n = n;
+        this.setLayout(null);
+        if(n.getModelo() == Modelos.APOLO_11.getmodelo()) {
+            this.setPreferredSize(new Dimension(760, 570));
+        }
+        else if (n.getModelo() == Modelos.ARCA_DEL_VACÍO.getmodelo()) {
+            this.setPreferredSize(new Dimension(750, 594));
+        }
+        else if (n.getModelo() == Modelos.HALCÓN_MILENARIO.getmodelo()) {
+            this.setPreferredSize(new Dimension(750, 500));
+        }
+        else if (n.getModelo() == Modelos.MORNING_STAR.getmodelo()) {
+            this.setPreferredSize(new Dimension(750, 481));
+        }
+        else if (n.getModelo() == Modelos.UES_CONTACT_LIGHT.getmodelo()) {
+            this.setPreferredSize(new Dimension(499, 261));
+        }
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(n.getModelo() == Modelos.APOLO_11.getmodelo()) {
+            ImageIcon APOLO_11_InteriorIcon = new ImageIcon(getClass().getClassLoader().getResource("Apolo_11_Interior.png"));
+            g.drawImage(APOLO_11_InteriorIcon.getImage(), 0, 0, null);
+        }
+        else if (n.getModelo() == Modelos.ARCA_DEL_VACÍO.getmodelo()) {
+            ImageIcon ARCA_DEL_VACÍO_InteriorIcon = new ImageIcon(getClass().getClassLoader().getResource("BACS.png"));
+            g.drawImage(ARCA_DEL_VACÍO_InteriorIcon.getImage(), 0, 0, null);
+        }
+        else if (n.getModelo() == Modelos.HALCÓN_MILENARIO.getmodelo()) {
+            ImageIcon HALCÓN_MILENARIO_InteriorIcon = new ImageIcon(getClass().getClassLoader().getResource("Halcón_Milenario_Interior.png"));
+            g.drawImage(HALCÓN_MILENARIO_InteriorIcon.getImage(), 0, 0, null);
+        }
+        else if (n.getModelo() == Modelos.MORNING_STAR.getmodelo()) {
+            ImageIcon MORNING_STAR_InteriorIcon = new ImageIcon(getClass().getClassLoader().getResource("Morning_Star_Interior.png"));
+            g.drawImage(MORNING_STAR_InteriorIcon.getImage(), 0, 0, null);
+        }
+        else if (n.getModelo() == Modelos.UES_CONTACT_LIGHT.getmodelo()) {
+            ImageIcon UES_CONTACT_LIGHT_InteriorIcon = new ImageIcon(getClass().getClassLoader().getResource("UES_Contact_Light_Interior.png"));
+            g.drawImage(UES_CONTACT_LIGHT_InteriorIcon.getImage(), 0, 0, null);
+        }
+    }
+}
--- src/main/java/parte_gráfica/VentanaInterior.java
+++ src/main/java/parte_gráfica/VentanaInterior.java
@@ -1,31 +1,33 @@
 package parte_gráfica;
 import parte_lógica.*;
 import javax.swing.*;
+import java.awt.*;
 /**Ventana que representa el interior de las naves espaciales
  * @author Gustavo González
- * @version versión 1, 28 de junio 2024*/
+ * @version versión 2, 30 de junio 2024*/
 public class VentanaInterior extends JFrame {
     /**Constructor de VentanaInterior*/
     public VentanaInterior(NaveEspacial n) {
-        this.setLayout(null);
+        this.setLayout(new BorderLayout());
+        this.add(new PanelInterior(n), BorderLayout.CENTER);
         if(n.getModelo() == Modelos.APOLO_11.getmodelo()) {
-            this.setSize(300, 900);
+            this.setSize(760, 570);
             this.setTitle("Apolo 11");
         }
         else if (n.getModelo() == Modelos.ARCA_DEL_VACÍO.getmodelo()) {
-            this.setSize(600, 500);
+            this.setSize(750, 594);
             this.setTitle("Arca del Vacío");
         }
         else if (n.getModelo() == Modelos.HALCÓN_MILENARIO.getmodelo()) {
-            this.setSize(800, 600);
+            this.setSize(750, 500);
             this.setTitle("Halcón Milenario");
         }
         else if (n.getModelo() == Modelos.MORNING_STAR.getmodelo()) {
-            this.setSize(900, 900);
+            this.setSize(750, 481);
             this.setTitle("Morning Star");
         }
         else if (n.getModelo() == Modelos.UES_CONTACT_LIGHT.getmodelo()) {
-            this.setSize(300, 600);
+            this.setSize(499, 261);
             this.setTitle("UES Contact Light ");
         }
     }
