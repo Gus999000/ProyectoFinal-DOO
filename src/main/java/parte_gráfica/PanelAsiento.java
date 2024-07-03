24ca8fe835dcfe77d7b6c2c137e7570ee4a7e950
gustavo <gustavo.gonzalez8190@gmail.com>
2024-07-03 01:10:41 -0400

    Añadido el botón, el número va en el botón por temas estéticos, el método para reservar aún no funciona.

--- src/main/java/parte_gráfica/PanelAsiento.java
+++ src/main/java/parte_gráfica/PanelAsiento.java
@@ -2,24 +2,28 @@ package parte_gráfica;
 import parte_lógica.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 /**Panel que representa un asiento
  * @author Gustavo González
- * @version versión 4, 01 de julio 2024*/
-public class PanelAsiento extends JPanel {
+ * @version versión 5, 03 de julio 2024*/
+public class PanelAsiento extends JPanel implements ActionListener {
     /**Instancia lógica del asiento*/
     private Asiento a;
+    /***/
+    private JButton X;
     /**Constructor de Panelasiento
      * @param a Asiento*/
     public PanelAsiento(Asiento a) {
         this.a = a;
-        this.setPreferredSize(new Dimension(100,100));
-        this.add(new JLabel(String.valueOf(a.getNúmero())));
+        this.setPreferredSize(new Dimension(50,50));
+        this.X = new JButton(String.valueOf(a.getNúmero()));
+        this.add(X);
     }
     /**Método que cambia la disponibilidad a "no disponible", revalida el panel y lo repinta*/
     public void reservarPanel() {
         this.a.reservar();
-        revalidate();
-        repaint();
+        this.revalidate();
+        this.repaint();
     }
     @Override
     public void paintComponent(Graphics g) {
@@ -27,8 +31,13 @@ public class PanelAsiento extends JPanel {
         if(a.getDisponibilidad() == true) {
             this.setBackground(Color.green);
         }
-        else if (a.getDisponibilidad() == false) {
+        else {
             this.setBackground(Color.red);
+        }
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == X) {
         }
     }
 }
